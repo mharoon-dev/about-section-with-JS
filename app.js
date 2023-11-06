29bf3ff43a4f3741d6d78715fe570aecd2fb237f
mharoon-dev <agar3d@gmail.com>
2023-11-06 16:42:55 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,91 @@
+let history = document.querySelector('.history')
+let vision = document.querySelector('.vision')
+let goals = document.querySelector('.goals')
+let myMainRow = document.querySelector('.myMainRow')
+
+
+let aboutCards = [
+    { // history
+        name:'history',
+        image:'./history.webp',
+        details:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores maiores ducimus  suscipit aliquam, illo itaque odit eveniet distinctio iure quaerat maxime consequatur  perferendis ipsam quibusdam earum architecto? Distinctio, id ea?',
+    },
+    { // vision
+        name:'vision',
+        image:'./vision.jpg',
+        details:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores maiores ducimus  suscipit aliquam, illo itaque odit eveniet distinctio iure quaerat maxime consequatur  perferendis ipsam quibusdam earum architecto? Distinctio, id ea?',
+    },
+    { // goals
+        name:'goals',
+        image:'./goals.jpg',
+        details:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores maiores ducimus  suscipit aliquam, illo itaque odit eveniet distinctio iure quaerat maxime consequatur  perferendis ipsam quibusdam earum architecto? Distinctio, id ea?',
+    }
+]
+
+
+function btn(btnName) {
+    myMainRow.innerHTML = ''
+
+    let filtration = aboutCards.filter( (card) => {
+        if (card.name ==  btnName) {
+            return true
+        }
+    });
+
+    if (filtration == "") {
+        myMainRow.innerHTML = 
+        `<div class="col-md-6 col-12">
+                <img class="w-100" src="${aboutCards[0].image}">
+            </div>
+            <div class="col-md-6 col-12 m-auto">
+                <div class="row">
+                    <div class="col-4 ">
+                        <button onclick="btn('history')" class="w-100 history">history</button>
+                    </div>
+                    <div class="col-4 ">
+                        <button onclick="btn('vision')" class="w-100 vision">vision</button>
+                    </div>
+                    <div class="col-4 ">
+                        <button onclick="btn('goals')" class="w-100 goals">goals</button>
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col">
+                        <h6>${aboutCards[0].name}</h6>
+                        <p>${aboutCards[0].details}</p>
+                    </div>
+                </div>
+            </div>`
+    }
+
+    filtration.forEach( (filterCard) => {
+        myMainRow.innerHTML = 
+        `<div class="col-md-6 col-12">
+        <img class="w-100" src="${filterCard.image}">
+    </div>
+    <div class="col-md-6 col-12 m-auto">
+        <div class="row">
+            <div class="col-4 ">
+                <button onclick="btn('history')" class="w-100 history">history</button>
+            </div>
+            <div class="col-4 ">
+                <button onclick="btn('vision')" class="w-100 vision">vision</button>
+            </div>
+            <div class="col-4 ">
+                <button onclick="btn('goals')" class="w-100 goals">goals</button>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col">
+                <h6>${filterCard.name}</h6>
+                <p>${filterCard.details}</p>
+            </div>
+        </div>
+    </div>`
+
+    })
+}
+
+btn()
